6925e1a73b5bc985ca563ab3f3fed0e335aff506
HamidReza Shareghzade <72819222+DeveloperPY-git@users.noreply.github.com>
2020-12-12 04:07:56 +0330

    Add comma to welcome-bot.py

--- welcome-bot.py
+++ welcome-bot.py
@@ -34,9 +34,9 @@ async def on_member_join(member):
     except:
         print("Couldn't message " + member.name)
     embed=discord.Embed(
-        title="Welcome "+member.name+"!"
-        description="We're so glad you're here!"
-        color=discord.Color.green()
+        title="Welcome "+member.name+"!",
+        description="We're so glad you're here!",
+        color=discord.Color.green(),
     )
         
     role = discord.utils.get(member.server.roles, name="name-of-your-role") #  Gets the member role as a `role` object
@@ -48,7 +48,7 @@ async def on_member_leave(member):
     print("Recognised that a member called " + member.name + " left")
     embed=discord.Embed(
         title="😢 Goodbye "+member.name+"!",
-        description="Until we meet again old friend." # A description isn't necessary, you can delete this line if you don't want a description.
-        color=discord.Color.red() # There are lots of colors, you can check them here: https://discordpy.readthedocs.io/en/latest/api.html?highlight=discord%20color#discord.Colour
+        description="Until we meet again old friend.", # A description isn't necessary, you can delete this line if you don't want a description.
+        color=discord.Color.red(), # There are lots of colors, you can check them here: https://discordpy.readthedocs.io/en/latest/api.html?highlight=discord%20color#discord.Colour
     )
 client.run('token') 
